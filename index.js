ea2bbc340b7c5b79ba9f74fbb565b330e8a647da
denisecaze <denisecaze@hotmail.com>
2018-09-03 14:28:27 -0300

    correções nyc e alteração arquivo eslintrc

--- index.js
+++ index.js
@@ -4,24 +4,20 @@ exports.getLinksFromMd = text => {
   if (text == null) {
     throw new Error("Erro: parâmetro vazio.");
   }
-  if (text === "") {
-    throw new Error("Erro: o parâmetro não pode ser uma string vazia.");
-  } 
-
-  if (typeof text === "number") {
-    throw new Error("Erro: números não são válidos.");    
-  }
-  
-  if (typeof text === "string") {
-    let result = [];
-    let regExpMarkDownUrl = new RegExp(/(https?:\/\/)?(www\.)?[a-z0-9]+(\.\w{2,3}){1,2}((\/\w+)?){1,}/g);
-    let regExpMarkDownText = new RegExp(/(?<=\[).*?(?=\])/g);
-    let url = text.match(regExpMarkDownUrl);
-    let markDown = text.match(regExpMarkDownText);
-    _.zipWith(url, markDown, function(a, b) {
-      let obj = { "href": a, "text": b};
-      result.push(obj);
-    });
-    return url ? result : [];
-  }
+  switch(typeof text) {
+    case "number":
+      throw new Error("Erro: números não são válidos.");
+    case "string":
+      if (text.length === 0) {
+        throw new Error("Erro: o parâmetro não pode ser uma string vazia.");
+      } 
+      let regExpMarkDownUrl = new RegExp(/(https?:\/\/)?(www\.)?[a-z0-9]+(\.\w{2,3}){1,2}((\/\w+)?){1,}/g);
+      let regExpMarkDownText = new RegExp(/(?<=\[).*?(?=\])/g);
+      let url = text.match(regExpMarkDownUrl);
+      let markDown = text.match(regExpMarkDownText);
+      let result = _.zipWith(url, markDown, function(a, b) {
+        return { "href": a, "text": b};
+      });
+    return result;
+  };
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,27 +1,25 @@
-const mocha = require("mocha"); 
 const chai = require("chai");
 const library = require("../index"); 
 const expect = chai.expect;
 
 describe("getLinksFromMd", () => {
-
   describe("when there is no parameter", () => {
     it("should return an error", () => {
-      let nullParameter = () => { library.getLinksFromMd() };
+      let nullParameter = () => { library.getLinksFromMd(); };
       expect(nullParameter).throw("Erro: parâmetro vazio.");
     });
   });
 
   describe("when the parameter is an empty string", () => {
     it("should return an error", () => {
-      let EmptyStrParameter = () => { library.getLinksFromMd("") }; 
+      let EmptyStrParameter = () => { library.getLinksFromMd(""); }; 
       expect(EmptyStrParameter).throw("Erro: o parâmetro não pode ser uma string vazia.");
     });
   });
 
   describe("when the parameter is a number", () => {
     it("should return an error", () => {
-      let numberParameter = () => { library.getLinksFromMd(0) };
+      let numberParameter = () => { library.getLinksFromMd(0); };
       expect(numberParameter).throw("Erro: números não são válidos.");
     });
   });
@@ -41,21 +39,13 @@ describe("getLinksFromMd", () => {
 
   describe("when the parameter has two links", () => {
     it("should return an url and markdown links object array", () => {
-      expect(library.getLinksFromMd("temos dois markdowns e dois links: [google] (http://www.google.com), [youtube] (www.youtube.com.br/videos)")).to.deep.equal([ { href: 'http://www.google.com', text: 'google' }, { href: 'www.youtube.com.br/videos', text: 'youtube' } ]);
+      expect(library.getLinksFromMd("temos dois markdowns e dois links: [google] (http://www.google.com), [youtube] (www.youtube.com.br/videos)")).to.deep.equal([ { href: "http://www.google.com", text: "google" }, { href: "www.youtube.com.br/videos", text: "youtube" } ]);
     });
   });
 
   describe("when the parameter has more than two links", () => {
     it("should return an url and markdown links object array", () => {
-      expect(library.getLinksFromMd("temos muitos markdowns e vários links: [google] (http://www.google.com), [youtube] (www.youtube.com.br/videos), [hotmail] (https://hotmail.com), [twitter] (http://www.twitter.com.br/feed/user/friends)")).to.deep.equal([ { href: 'http://www.google.com', text: 'google' }, { href: 'www.youtube.com.br/videos', text: 'youtube' }, { href: 'https://hotmail.com', text: 'hotmail'}, { href: 'http://www.twitter.com.br/feed/user/friends', text: 'twitter'} ]);
+      expect(library.getLinksFromMd("temos muitos markdowns e vários links: [google] (http://www.google.com), [youtube] (www.youtube.com.br/videos), [hotmail] (https://hotmail.com), [twitter] (http://www.twitter.com.br/feed/user/friends)")).to.deep.equal([ { href: "http://www.google.com", text: "google" }, { href: "www.youtube.com.br/videos", text: "youtube" }, { href: "https://hotmail.com", text: "hotmail"}, { href: "http://www.twitter.com.br/feed/user/friends", text: "twitter"} ]);
     });
   });
-
 });
-
-
-// TALVEZ COLOCAR O DESCRIBE EM CADA TESTE, PARA FICAR MAIS CLARO
-// MELHORAR, REDUZIR E UNIFICAR CÓDIGO. TUDO EM LODASH?
-// FAZER README
-// VERIFICAR CONFIGURAÇÕES DO ERLINT E VER COMO ELE É USADO PELOS MENTORES (SABER SE O MEU ESTÁ OK) VER INDENT 2 OU 4
-// RETIRAR NYC_OUTPUT DO GITHUB
